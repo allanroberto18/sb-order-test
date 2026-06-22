0a7daa9169070097975c4e5e022085765784ee86
allan roberto <allanroberto18@gmail.com>
2026-06-22 09:14:01 -0300

    add domain layer for ai support

---
+++ src/main/java/br/com/alr/order/support/domain/SupportIntent.java
@@ -0,0 +1,8 @@
+package br.com.alr.order.support.domain;
+
+public enum SupportIntent {
+  GENERAL_POLICY_QUESTION,
+  ORDER_STATUS_QUESTION,
+  CANCELLATION_REQUEST,
+  PROMPT_INJECTION_ATTEMPT
+}
---
+++ src/main/java/br/com/alr/order/support/domain/SupportPolicy.java
@@ -0,0 +1,4 @@
+package br.com.alr.order.support.domain;
+
+public record SupportPolicy(String context, String rule) {
+}
